ecf6dc98a48bf1dc3dbd6589ee6ff3f53b9a940f
ArnoDG <arno.de.greef@hotmail.com>
2021-11-15 22:22:15 +0100

    update spawning crates (work in progress update)

--- crate.asm
+++ crate.asm
@@ -6,6 +6,7 @@ ASSUME cs:_TEXT,ds:FLAT,es:FLAT,fs:FLAT,gs:FLAT
 include "crate.inc"
 include "drawer.inc"
 include "physics.inc"
+include "utils.inc"
 
 CRATE_WIDTH equ 10
 CRATE_HEIGHT equ 10
@@ -32,12 +33,26 @@ PROC Crate_spawn_new_crate
 
     ;; make new crate
     xor eax, eax
+    call Utils_get_next_active_index, OFFSET crates_active, CRATES_MAX_COUNT
+    cmp eax, -1
+    je @@return ;; check if not full and fail silently
+    ;; set active
+    call Utils_set_active, OFFSET crates_active, eax, 1
+    ;; get new active crate
+    call Utils_get_if_active, OFFSET crates_active, OFFSET crates_objects, eax
+
+ ;   mov edi, OFFSET crates_objects
+ ;   mov edi, [edi + 4*eax]
+   
+    ;; making crate spawn on same x-coordinate as player
+    mov edi, eax ; destination pointer with active crate in eax
+    xor eax, eax
     mov esi, [@@player_ptr]
-    mov edi, OFFSET crate3
     mov ax, [(Drawable PTR esi).x]
     mov [(Drawable PTR edi).x], ax
+    ;add new crate to physics
+    call Physics_add_dynamic, edi
 
-    call Physics_add_dynamic, OFFSET crate3
     ;;reset timer
     mov [spawn_crate_timer], 0
     jmp @@return
@@ -54,25 +69,42 @@ PROC Crate_spawn_new_crate
 ENDP
 
 PROC Crate_update
-    USES esi
+    USES esi, ecx, eax
 
 ;; call Crate_spawn_new_crate --> called from Player_update
 
-    mov esi, OFFSET crate3
-    call Drawer_draw, esi
+;; updating active crates
+    mov ecx, CRATES_MAX_COUNT
+@@loop_crates:
+    push ecx
+    dec ecx ; ecx in [0-len)
 
+    call Utils_get_if_active, OFFSET crates_active, OFFSET crates_objects, ecx
+    test eax, eax ; test if active
+    jz @@end_crates ; if NULL -> not active
+    
+    ;; update current crate
+    mov esi, eax ; PTR currently in eax
+    call Drawer_draw, esi
     call Physics_apply_gravity_with_collision, esi, 1
+
+@@end_crates:
+    pop ecx
+    loop @@loop_crates
    
 
     ret
 ENDP
 
 DATASEG
-;;crate1 Drawable <20,150,100,10,offset crateSprite>
-;;crate2 Drawable <60,140,10,10,offset crateSprite>
 spawn_crate_timer dd 0
-crate3 Drawable <120,10,10,10,offset crateSprite>
+;crate1 Drawable <120,10,CRATE_WIDTH,CRATE_HEIGHT,offset crateSprite>
+;crate2 Drawable <120,10,CRATE_WIDTH,CRATE_HEIGHT,offset crateSprite>
 crateSprite db 3000 DUP(3Fh)
+crates_active  db CRATES_MAX_COUNT/8  DUP(0)
 
+UDATASEG
+
+crates_objects  Drawable CRATES_MAX_COUNT  DUP(<120,10,CRATE_WIDTH,CRATE_HEIGHT,offset crateSprite>)
 
 end
